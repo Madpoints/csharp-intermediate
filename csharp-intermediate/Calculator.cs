f9b20bc5f57c1eada4b298552d59fe78939e0c60
John Furtado <jf8846@gmail.com>
2017-12-07 19:28:00 -0500

    Methods

---
+++ csharp-intermediate/Calculator.cs
@@ -0,0 +1,16 @@
+﻿namespace csharp_intermediate
+{
+    public class Calculator
+    {
+        public int Add(params int[] numbers)
+        {
+            var sum = 0;
+            foreach(var number in numbers)
+            {
+                sum += number;
+            }
+
+            return sum;
+        }
+    }
+}
--- csharp-intermediate/Program.cs
+++ csharp-intermediate/Program.cs
@@ -11,15 +11,49 @@ namespace csharp_intermediate
     {
         static void Main(string[] args)
         {
-            var customer = new Customer();
-            customer.Id = 1;
-            customer.Name = "John";
+            try
+            {
+                var num = int.Parse("abc");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Conversion failed.");
+            }
 
-            var order = new Order();
-            customer.Orders.Add(order);
+            int number;
+            var result = int.TryParse("abc", out number);
+            if (result)
+                Console.WriteLine(number);
+            else
+                Console.WriteLine("Conversion failed.");
+        }
 
-            Console.WriteLine(customer.Id);
-            Console.WriteLine(customer.Name);
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3));
+            Console.WriteLine(calculator.Add(1, 2, 3, 4));
+            Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 4, 5 }));
+        }
+
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(null);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+                point.Move(100, 200);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("An unexpected error occured");
+            }
         }
     }
 }
