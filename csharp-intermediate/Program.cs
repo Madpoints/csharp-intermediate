656479bd4e90b0796b5a611b829aaf1da6177d2d
John Furtado <jf8846@gmail.com>
2017-12-11 18:09:33 -0500

    Properties

--- csharp-intermediate/Program.cs
+++ csharp-intermediate/Program.cs
@@ -5,28 +5,14 @@ using System.Threading.Tasks;
 
 namespace csharp_intermediate
 {
-    public class Person
-    {
-        private DateTime _birthdate;
-
-        public void SetBirthDate(DateTime birthdate)
-        {
-            _birthdate = birthdate;
-        }
-
-        public DateTime GetBirthdate()
-        {
-            return _birthdate;
-        }
-    }
 
     partial class Program
     { 
         static void Main(string[] args)
         {
-            var person = new Person();
-            person.SetBirthDate(new DateTime(1985, 12, 31));
-            Console.WriteLine(person.GetBirthdate());
+            var person = new Person(new DateTime(1985, 12, 31));
+
+            Console.WriteLine(person.Age);
         }
     }
 }
