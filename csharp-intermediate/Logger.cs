c832b66bd93b0f92b451cf1b486ada4f6989e5ce
John Furtado <jf8846@gmail.com>
2017-12-12 19:46:58 -0500

    Composition

---
+++ csharp-intermediate/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_intermediate
+{
+    public class Logger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
--- csharp-intermediate/Program.cs
+++ csharp-intermediate/Program.cs
@@ -4,13 +4,19 @@ using System.Threading.Tasks;
 
 namespace csharp_intermediate
 {
+
     partial class Program
     { 
         static void Main(string[] args)
         {
-            var text = new Text();
-            text.Width = 100;
-            text.Copy();
+            var dbMigrator = new DbMigrator(new Logger());
+
+            var logger = new Logger();
+            var installer = new Installer(logger);
+
+            dbMigrator.Migrate();
+
+            installer.Install();
         }
     }
 }
