9019b6266336888d8fa1b9310a34126a800d1ccd
John Furtado <jf8846@gmail.com>
2017-12-14 19:23:49 -0500

    Access Modifiers Cont.

--- csharp-intermediate/Program.cs
+++ csharp-intermediate/Program.cs
@@ -1,18 +1,7 @@
-﻿using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Amazon;
 
 namespace csharp_intermediate
 {
-    public class GoldCustomer : Customer
-    {
-        // Calculate rating no longer exists, this class is now broken
-        public void OfferVoucher()
-        {
-            var rating = this.CalculateRating(excludeOrders: true);
-        }   
-    }
-
     partial class Program
     { 
         static void Main(string[] args)
